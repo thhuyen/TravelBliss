c9e0acd75b144f4d3dc036bb76f6007f2b5ded32
Thanh Huyền Nguyễn <97439051+thhuyen@users.noreply.github.com>
2023-11-11 22:18:14 +0700

    feat(BFF): write schema and query for users, destinations and FAQs (#2)

---
+++ app/server/src/resolver/resolver.ts
@@ -0,0 +1,79 @@
+import axios, { AxiosRequestConfig } from "axios";
+import { get } from 'lodash'
+
+const queries = {
+    users: {
+        api: "http://localhost:3000/users",
+        query: {
+            "operationName": "getUsers",
+            "query": ` query {
+                getUsers {
+                    fullName
+                    phoneNumber
+                    email
+                }
+            }`
+        }
+    },
+    popularDestination: {
+        api: "http://localhost:3000/popularDestination",
+        query: {
+            "operationName": "getPopularDestinations",
+            "query": ` query {
+                getPopularDestinations {
+                    place
+                    url
+                }
+            }`
+        }
+    },
+    FAQs: {
+        api: "http://localhost:3000/FAQs",
+        query: {
+            "operationName": "getPopularDestinations",
+            "query": ` query {
+                getFAQs {
+                    id
+                    question
+                    answer
+                }
+            }`
+        }
+    }
+}
+
+const headers = {
+    "content-type": "application/json",
+    "Authorization": "<token>"
+};
+
+
+const handleCallAPI = async (url: string, method: string, headers: object, data: any) => {
+    try {
+        return get(await axios({
+            url: url,
+            method: method,
+            headers: headers,
+            data: data
+        }), "data");
+    } catch (error) {
+        throw new Error(`${error}`);
+    }
+};
+
+export const resolvers = {
+    Query: {
+        getUsers: () => {
+            return handleCallAPI(get(queries, "users.api"), 'get', headers, get(queries, "users.query"))
+        },
+
+        getPopularDestinations: () => {
+            return handleCallAPI(get(queries, "popularDestination.api"), 'get', headers, get(queries, "popularDestination.query"))
+
+        },
+
+        getFAQs: () => {
+            return handleCallAPI(get(queries, "FAQs.api"), 'get', headers, get(queries, "FAQs.query"))
+        }
+    }
+}
