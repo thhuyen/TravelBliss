7f95b6261c82c2e36094ed3d3ee9cf4d5718059a
Thanh Huyền Nguyễn <20520207@gm.uit.edu.vn>
2023-11-11 22:17:16 +0700

    feat(BFF): write schema and query for users, destinations and FAQs

--- app/server/index.ts
+++ app/server/index.ts
@@ -1,26 +1,14 @@
-// import express from "express";
-// import {ApolloServer} from "apollo-server";
+import { ApolloServer } from "apollo-server";
+import { typeDefs } from "./src/schema";
+import { resolvers } from "./src/resolver/resolver";
 
-// const typeDefs = null;
-// const resolvers = null;
+const port = process.env.PORT || 5000;
 
-// const server = new ApolloServer({
-//     typeDefs,
-//     resolvers
-// });
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
 
-// server.listen().then(({url}) => {
-//     console.log(`Server is ready at ${url}`)
-// })
-import express from "express";
-const graphql = require("express-graphql").graphqlHTTP;
-
-const app = express();
-const port = process.env.PORT || 8080;
-
-app.use("/", graphql({
-    schema: null, // TODO: Implement GraphQL schema
-}));
-app.listen(port, () => {
-    console.log(`GraphQL API listening on http://localhost:${port}/`);
+server.listen(port, () => {
+    console.log(`GraphQL is ready at http://localhost:${port}/`);
 });
--- app/server/src/schema/index.ts
+++ app/server/src/schema/index.ts
@@ -1,16 +1,29 @@
 import { gql } from "apollo-server-express";
 
 export const typeDefs = gql`
-    type Book {
-        id: ID
-        name: String,
-        genre: String
+    type User {
+        fullName: ID
+        phoneNumber: String
+        email: String
+    }
+    
+    type PopularDestination {
+        place: String
+        url: String
+    }
+
+    type FAQ {
+        id: String
+        question: String
+        answer: String
     }
 
     # ROOT TYPE
 
     type Query {
-        books: [Book]
+        getUsers: [User]
+        getPopularDestinations: [PopularDestination]
+        getFAQs: [FAQ]
     }
 `
 
