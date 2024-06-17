c76813b4dd001a02f05ec6228d1ab9dc6abd174b
Thanh Huyền Nguyễn <20520207@gm.uit.edu.vn>
2024-06-17 12:27:46 +0700

    feat: graphQL for checkuser

--- app/server/src/modules/schemas.ts
+++ app/server/src/modules/schemas.ts
@@ -1,4 +1,4 @@
 import { typeDefs as createUserTypeDefs } from "../modules/createUser/createUser-schema";
 import { typeDefs as getUserTypeDefs } from "../modules/getUser/getUser.schema";
-
-export default [createUserTypeDefs, getUserTypeDefs]
+import { typeDefs } from "./checkUser/checkUser-schema";
+export default [createUserTypeDefs, getUserTypeDefs, typeDefs]
